eacfcf19a06436d08b9a818ae9ae782913376a28
CM03LP <maginn.domingo@ing.com>
2022-11-10 10:00:10 +0800

    TODO: Make datastax mapper work. To springboot

---
+++ README.md
@@ -0,0 +1,11 @@
+# Maven build to Datastax Object mapper in Scala 
+
+## Objectives
+
+* Demonstrate how to use the Java Driver object mapper to replace the tedious work of DAO recreation
+  in Scala. Reference the
+  [documentation](https://docs.datastax.com/en/developer/java-driver/latest/manual/mapper/mapper/)
+  for details about the object mapper.
+* Demonstrate how to model entities as Scala case classes, and DAOs and mappers as Scala traits.
+* Demonstrate how to integrate the annotation processor on maven build.
+* This project is taken from this repo [GitHub] (https://github.com/DataStax-Examples/object-mapper-jvm) . Thanks to the Author. 
---
+++ src/main/scala/com/datastax/examples/killrvideo/video/LatestVideo.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.examples.killrvideo.video
+
+import com.datastax.oss.driver.api.mapper.annotations.{ClusteringColumn, CqlName, Entity, PartitionKey}
+
+import java.time.Instant
+import java.util.UUID
+import scala.annotation.meta.field
+
+@Entity
+@CqlName("latest_videos")
+case class LatestVideo(@(PartitionKey@field) yyyymmdd: String,
+                       @(ClusteringColumn@field)(0) addedDate: Instant,
+                       @(ClusteringColumn@field)(1) videoid: UUID,
+                       userid: UUID,
+                       name: String,
+                       previewImageLocation: String)
